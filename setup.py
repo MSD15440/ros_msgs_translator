8797f147c989bbf43ce06fd65122f44c2d221940
Atticus Russell <60147768+atticusrussell@users.noreply.github.com>
2023-04-06 18:06:25 -0400

    get topics and message types from args (#4)

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'f64_to_multiarray = f64tomulti.f64_to_multiarray:main',
+            'ros_msgs_translator = ros_msgs_translator.ros_msgs_translator:main',
         ],
     },
 )
